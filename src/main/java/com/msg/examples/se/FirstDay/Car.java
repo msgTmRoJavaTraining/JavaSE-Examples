56e5ab9e7a52b29be643368f6e286e621fcb41da
maranalex <alexmaran98@gmail.com>
2019-07-08 11:45:02 +0300

    added Car class with 3 constructors

---
+++ src/main/java/com/msg/examples/se/FirstDay/Car.java
@@ -0,0 +1,31 @@
+package com.msg.examples.se.FirstDay;
+
+public class Car {
+    private int seats;
+    private String color;
+
+
+    public Car()
+    {
+        this.seats=4;
+        this.color="black";
+    }
+    public Car(String color)
+    {
+        this.color=color;
+        this.seats=4;
+    }
+    public Car(int seats,String color)
+    {
+        this.seats=seats;
+        this.color=color;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
--- src/main/java/com/msg/examples/se/FirstDay/FirstClass.java
+++ src/main/java/com/msg/examples/se/FirstDay/FirstClass.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class FirstClass {
    public static void main(String args[])
    {
+       /*
        Hamburger ham1=new Hamburger();
        Hamburger ham2=new Hamburger();
        Hamburger ham3=new Hamburger();
@@ -21,10 +22,16 @@ public class FirstClass {
        ham3.setWeight(200);
        ham3.setExpire("2020/01/01");
 
-
        System.out.println(ham1.getName()+ " "+ham1.getWeight());
        System.out.println("After raise(25): "+ham1.getName()+ " "+ham1.raise(25));
+*/
+       Car car1=new Car();
+       Car car2=new Car("green");
+       Car car3=new Car(2,"red");
 
+       System.out.println(car1.getColor()+ " "+car1.getSeats());
+       System.out.println(car2.getColor()+ " "+car2.getSeats());
+       System.out.println(car3.getColor()+ " "+car3.getSeats());
 
    }
 
