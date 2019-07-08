34f89f73532da340d30a21976cae31d303100209
maranalex <alexmaran98@gmail.com>
2019-07-08 10:26:21 +0300

    Odd numbers

--- src/main/java/com/msg/examples/se/FirstDay/FirstClass.java
+++ src/main/java/com/msg/examples/se/FirstDay/FirstClass.java
@@ -1,6 +1,32 @@
 package com.msg.examples.se.FirstDay;
 
 public class FirstClass {
+   public static void main(String args[])
+   {
+        //FOR
+       for(int i=0;i<10;i++)
+       {
+           if(i%2==0)continue;
+           System.out.println(i+" ");
+       }
+       int i=0;
+       //DO
+       do{
+           if(i%2!=0)
+               System.out.println(i+" ");
 
-    
+           i=i+1;
+       }while(i<10);
+    i=0;
+       //WHILE
+       while(i!=10){
+
+           if(i%2!=0)
+               System.out.println(i+" ");
+           i=i+1;
+       }
+
+
+   }
+
 }
