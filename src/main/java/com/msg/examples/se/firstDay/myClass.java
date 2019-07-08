b7cac68a058e7cf3f6af63f84a88c19208d3f712
Mihaescu Razvan <razvan.mihaescu@student.upt.ro>
2019-07-08 10:20:33 +0300

    First exercise done.

--- src/main/java/com/msg/examples/se/firstDay/myClass.java
+++ src/main/java/com/msg/examples/se/firstDay/myClass.java
@@ -1,4 +1,31 @@
 package com.msg.examples.se.firstDay;
 
 public class myClass {
-}
+    public static void main(String[] args) {
+        int sum=0;
+        int i=0;
+        for(i=1;i<11;i++)
+        {
+            if(i%2!=0)
+                sum+=i;
+        }
+
+        System.out.println(sum);
+
+        while(++i<=10)
+        {
+            if(i%2==0) continue;
+            sum+=i;
+        }
+
+        System.out.println(sum);
+
+        do {
+            if(i%2==0) continue;
+            sum+=i;
+
+        }while (++i<10);
+
+        System.out.println(sum);
+    }
+    }
