892607bd40e7de629f7d3d8483e6faf4c289247d
Mihaescu Razvan <razvan.mihaescu@student.upt.ro>
2019-07-09 11:12:03 +0300

    Collections exercises done.

--- src/main/java/com/msg/examples/se/collections/CollectionsUtil.java
+++ src/main/java/com/msg/examples/se/collections/CollectionsUtil.java
@@ -1,7 +1,6 @@
 package com.msg.examples.se.collections;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class CollectionsUtil {
 
@@ -14,7 +13,7 @@ public class CollectionsUtil {
   }
 
   public static <T> void forEachExternalCollection(Collection<T> collection) {
-    for(T element : collection) {
+    for (T element : collection) {
       System.out.print(element + " ");
     }
 
@@ -27,7 +26,55 @@ public class CollectionsUtil {
       T element = iterator.next();
       System.out.print(element + " ");
     }
-
     System.out.print("\n");
+  }
+
+  public static void removeFirstOccurence(ArrayList<Integer> list, int specified_element) {
+    Iterator<Integer> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Integer element = iterator.next();
+      if (element.equals(specified_element)) {
+        iterator.remove();
+      }
+    }
+  }
+
+  public static void removeGivenLength(ArrayList<String> ArrayListofStrings, int givenLength) {
+    Iterator<String> iterator = ArrayListofStrings.iterator();
+    while (iterator.hasNext()) {
+      String value = iterator.next();
+      if (value.length() == givenLength) {
+        iterator.remove();
+      }
+    }
+  }
+
+  public static void insertFirstAndLast(LinkedList<Integer> list, Integer[] elementsToAdd) {
+    for (Integer i : elementsToAdd
+    ) {
+      list.addFirst(i);
+    }
+
+    for (Integer i : elementsToAdd
+    ) {
+      list.addLast(i);
+    }
+  }
+
+  public static void iterateThroughHashSet(HashSet<Integer> hashSet) {
+    Iterator<Integer> it = hashSet.iterator();
+    while (it.hasNext()) {
+      System.out.println(it.next());
+    }
+  }
+
+  public static void getTheWord(Map<Integer,String> map,int value)
+  {
+    if(map.containsKey(value))
+    {
+      map.get(value);
+    }
+    else
+      System.out.println("The key: -"+value+"- doesn't exist!");
   }
 }
--- src/main/java/com/msg/examples/se/collections/Lists.java
+++ src/main/java/com/msg/examples/se/collections/Lists.java
@@ -5,24 +5,75 @@ import java.util.*;
 public class Lists {
 
   public static void main(String args[]) {
-    ArrayList<Integer>  array = new ArrayList<>();
+    ArrayList<String> string_array = new ArrayList<>();
+    ArrayList<Integer> array = new ArrayList<>();
+    LinkedList<Integer> linked = new LinkedList<>();
+    HashSet<Integer> hash = new HashSet<>();
+    Map<Integer,String> map = new TreeMap<>();
+
+
     array.add(8);
     array.add(0, 5);
     array.add(2);
     array.add(4);
-    CollectionsUtil.iterateCollection(array);
-    System.out.println("Min = " + Collections.min(array));
 
-    LinkedList<Integer> linked = new LinkedList<>(array);
-    linked.addLast(10);
-    linked.addFirst(10);
-    CollectionsUtil.forEachExternalCollection(linked);
-    System.out.println("Max = " + Collections.max(linked));
-    Collections.replaceAll(linked, 10, 7);
-    CollectionsUtil.forEachInternalCollection(linked);
-    System.out.println("Exists = " + Collections.binarySearch(linked, 7));
-    Collections.sort(linked);
-    System.out.println("Exists = " + Collections.binarySearch(linked, 7));
+    //Exercise 1
+    CollectionsUtil.removeFirstOccurence(array,5);
+    for (int i:array
+    ) {System.out.println(i);
+
+    }
+
+    //Exercise 2
+    System.out.println();
+    string_array.add("asd");
+    string_array.add("asdasd");
+    string_array.add("asdasdasd");
+    string_array.add("asd");
+    string_array.add("asdasd");
+    CollectionsUtil.removeGivenLength(string_array,3);
+    for (String i:string_array
+    ) {System.out.println(i);
+
+    }
+
+    //Exercise 3
+    System.out.println();
+    Integer[] elements = {2,3,4,5};
+    CollectionsUtil.insertFirstAndLast(linked,elements);
+    linked.forEach(System.out::println);
+
+    //Exercise 4
+    System.out.println();
+    hash.add(5);
+    hash.add(1);
+    hash.add(2);
+    CollectionsUtil.iterateThroughHashSet(hash);
+
+    //Exercise 5
+    System.out.println();
+    map.put(1,"one");
+    map.put(2,"two");
+    map.put(3,"three");
+    CollectionsUtil.getTheWord(map,2);
+    CollectionsUtil.getTheWord(map,3);
+    CollectionsUtil.getTheWord(map,7);
+
+//    CollectionsUtil.iterateCollection(array);
+//    System.out.println("Min = " + Collections.min(array));
+//
+//    LinkedList<Integer> linked = new LinkedList<>(array);
+//    linked.addLast(10);
+//    linked.addFirst(10);
+//    CollectionsUtil.forEachExternalCollection(linked);
+//    System.out.println("Max = " + Collections.max(linked));
+//    Collections.replaceAll(linked, 10, 7);
+//    CollectionsUtil.forEachInternalCollection(linked);
+//    System.out.println("Exists = " + Collections.binarySearch(linked, 7));
+//    Collections.sort(linked);
+//    System.out.println("Exists = " + Collections.binarySearch(linked, 7));
+
+
   }
 
 
