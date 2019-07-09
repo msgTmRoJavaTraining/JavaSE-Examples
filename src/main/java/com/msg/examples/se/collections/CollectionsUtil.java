761838f58212d732aff6c7e952d8737fac1e166b
maranalex <alexmaran98@gmail.com>
2019-07-09 11:20:47 +0300

    Collections

--- src/main/java/com/msg/examples/se/collections/CollectionsUtil.java
+++ src/main/java/com/msg/examples/se/collections/CollectionsUtil.java
@@ -1,7 +1,9 @@
 package com.msg.examples.se.collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class CollectionsUtil {
 
@@ -30,4 +32,27 @@ public class CollectionsUtil {
 
     System.out.print("\n");
   }
+  public static <T> void removeFirstOccurrence(ArrayList<T> collection,int toDelete){
+    if(collection.indexOf(toDelete)<0)System.out.println("No action performed");
+    else collection.remove(collection.indexOf(toDelete));
+
+
+  }
+  public static <T>  void removeGivenLength(ArrayList<T> collection, int len){
+    Iterator<T> iterator = collection.iterator();
+    while (iterator.hasNext()) {
+      if(((String)iterator.next()).length()==len)
+        iterator.remove();
+    }
+  }
+  public static <T> void addFirstAndLast(LinkedList collection, String toAdd) {
+    collection.addFirst(toAdd);
+    collection.addLast(toAdd);
+  }
+  public static <T> void associationWords() {
+
+
+  }
+
+
 }
--- src/main/java/com/msg/examples/se/collections/Lists.java
+++ src/main/java/com/msg/examples/se/collections/Lists.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Lists {
 
   public static void main(String args[]) {
+    /*
     ArrayList<Integer>  array = new ArrayList<>();
     array.add(8);
     array.add(0, 5);
@@ -23,6 +24,56 @@ public class Lists {
     System.out.println("Exists = " + Collections.binarySearch(linked, 7));
     Collections.sort(linked);
     System.out.println("Exists = " + Collections.binarySearch(linked, 7));
+     */
+
+    ArrayList ints=new ArrayList();
+    ints.add(10);
+    ints.add(21);
+    ints.add(11);
+    ints.add(7);
+    ints.add(40);
+    ints.add(1);
+    ints.add(3);
+    ints.add(10);
+    LinkedList strings=new LinkedList();
+    strings.add("Bill");
+    strings.add("Steve");
+    strings.add("Dy");
+    strings.add("George");
+    strings.add("Claus");
+    HashSet hSet=new HashSet();
+    hSet.add(10);
+    hSet.add(21);
+    hSet.add(11);
+    hSet.add(7);
+    hSet.add(40);
+    hSet.add(1);
+    Map mapz=new HashMap();
+    mapz.put(1,"NumbaUan");
+    mapz.put(2,"duoz");
+    mapz.put(3,"triiiii");
+    //ex1
+    CollectionsUtil.forEachExternalCollection(ints);
+    CollectionsUtil.removeFirstOccurrence(ints,10);
+    CollectionsUtil.forEachExternalCollection(ints);
+    //ex2
+    CollectionsUtil.forEachExternalCollection(strings);
+    //CollectionsUtil.removeGivenLength(strings,20);
+    //ex3
+    CollectionsUtil.addFirstAndLast(strings,"Upppa");
+    CollectionsUtil.addFirstAndLast(strings,"uhuuu");
+    CollectionsUtil.forEachExternalCollection(strings);
+
+    //ex4
+    System.out.println(hSet);
+
+    //ex5
+    System.out.println(mapz);
+    System.out.println(mapz.get(1));
+
+
+
+
   }
 
 
