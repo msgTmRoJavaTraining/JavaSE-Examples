dc114fb8ded08d0f4904c38457e9ff10ebdb70b3
Andrei Astanei <andreihao@gmail.com>
2019-07-10 14:30:04 +0300

    Clasa Horse din cadrul ultimului exercitiu

---
+++ src/main/java/com/msg/examples/se/thirdDay/Horse.java
@@ -0,0 +1,9 @@
+package com.msg.examples.se.thirdDay;
+
+public class Horse implements Animal{
+
+    @Override
+    public void run() {
+        System.out.println("The horse runs fast");
+    }
+}
