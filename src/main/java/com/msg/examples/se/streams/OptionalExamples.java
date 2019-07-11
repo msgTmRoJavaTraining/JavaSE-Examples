8c1941abbb3dcbf43b67a3876084adaa4f90ee0d
ionut.borozan <ionut.borozan@student.upt.ro>
2019-07-11 11:10:11 +0200

    Add new methods in OptionalExamples and Streams classes

--- src/main/java/com/msg/examples/se/streams/OptionalExamples.java
+++ src/main/java/com/msg/examples/se/streams/OptionalExamples.java
@@ -1,9 +1,51 @@
 package com.msg.examples.se.streams;
 
 import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
-@SuppressWarnings({"ConstantConditions", "ResultOfMethodCallIgnored"})
 public class OptionalExamples {
+    public void stringFromList(List lst){
+
+        System.out.println(lst.stream().reduce("" , (sum, lastLength)
+                          -> {
+                               if(sum == ""){if((int)lastLength % 2 == 0) {
+                                     return sum + "e" + lastLength;
+                                 }else{
+                                      return sum + "o"+lastLength ;
+                                      }
+                                  }else{if((int)lastLength % 2 == 0) {
+                                      return sum + ",e" + lastLength;
+                                        }else{
+                                      return sum + ",o"+lastLength ;
+                                                       }}}));
+    }
+    public static void infiniteStreamGenerator(){
+        System.out.println("\nStreaming 5 random doubles:");
+        Random randomGenerator = new Random();
+        Stream.iterate(ThreadLocalRandom.current().nextDouble(0,10), n -> ThreadLocalRandom.current().nextDouble(0,10)).limit(5).forEach(System.out::println);
+
+        System.out.println("\nCreating a list with 10 random doubles:");
+        ArrayList<Double> listOfRandomDoubles=new ArrayList<>();
+        Stream.iterate(randomGenerator.nextDouble()*10, n -> randomGenerator.nextDouble()*10).limit(10).forEach(value -> listOfRandomDoubles.add(value));
+        Iterator<Double> iterator=listOfRandomDoubles.iterator();
+
+        int cnt=0;
+        while (iterator.hasNext()){
+            cnt++;
+            System.out.println("Place: "+cnt+" - Value: "+iterator.next());
+        }
+
+        System.out.println("\nCreating an array with 20 random doubles:");
+        Integer[] cnt2=new Integer[]{0};
+        Double[] arrayOfRandomDoubles=new Double[21];
+        Stream.iterate(randomGenerator.nextDouble()*10, n -> randomGenerator.nextDouble()*10).limit(20).forEach(value -> { cnt2[0]++;arrayOfRandomDoubles[cnt2[0]]=value;});
+
+        for (int i=1;i< arrayOfRandomDoubles.length;i++)
+            System.out.println("Place in array: "+ i+" - Value: "+arrayOfRandomDoubles[i]);
+    }
+
     public static void main(String[] args) throws Exception {
         Optional<String> opt = Optional.of("mere");
         opt.ifPresent(name -> System.out.println(name.length()));
--- src/main/java/com/msg/examples/se/streams/Streams.java
+++ src/main/java/com/msg/examples/se/streams/Streams.java
@@ -1,5 +1,5 @@
 package com.msg.examples.se.streams;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalInt;
@@ -9,13 +9,14 @@ import java.util.stream.Stream;
 
 public class Streams {
     private static final Logger LOGGER = Logger.getLogger(Streams.class.getPackage().getName());
+
     public static void main(String[] args) {
         String[] strs = {"ana", "are", "mere"};
         //count the number of elements of the str array
         System.out.println(Arrays.stream(strs).count());
 
         //total number of characters of str array
-        System.out.println(Stream.of(strs).map(String::length).reduce(0 , (sum, lastLength) -> sum + lastLength));
+        System.out.println(Stream.of(strs).map(String::length).reduce(0, (sum, lastLength) -> sum + lastLength));
 
         List<String> strings = Arrays.asList(strs);
         //print all elements of the strings array having the length different than 3
@@ -44,5 +45,22 @@ public class Streams {
                     return a + b;
                 });
         System.out.println(reducedParallel + " combiner is called with parallel streams");
+
+
+        System.out.println("------------------------------------------------");
+
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(20);
+        myList.add(3);
+        myList.add(14);
+        OptionalExamples ex = new OptionalExamples();
+        ex.stringFromList(myList);
+        //myString a method from OptionalExample class which is used to add
+        //e in front of the even numbers and o in front of the odd numbers
+
+
+        ex.infiniteStreamGenerator();
+
+
     }
 }
