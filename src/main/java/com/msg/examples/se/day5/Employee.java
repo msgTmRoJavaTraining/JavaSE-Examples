6972264fefd80105109f8641c86aeeacf5341160
Andrei Astanei <andreihao@gmail.com>
2019-07-12 12:35:24 +0300

    Day 5, clasa pentru un singur angajat

---
+++ src/main/java/com/msg/examples/se/day5/Employee.java
@@ -0,0 +1,50 @@
+package com.msg.examples.se.day5;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@ToString
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @XmlAttribute(name = "employee_id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlElement(name = "salary")
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
