4caf85eb470f14486d82ac7f8b2e40b464fe188e
Andrei Sold <sold_andrei@yahoo.com>
2019-07-12 09:08:43 +0300

    Modified the JDBC examples to be suitable to the students setup

--- src/main/java/com/msg/examples/se/jdbc/JDBCBlobOperations.java
+++ src/main/java/com/msg/examples/se/jdbc/JDBCBlobOperations.java
@@ -4,37 +4,39 @@ import java.io.*;
 import java.sql.*;
 
 public class JDBCBlobOperations {
-  public static void main(String args[]) {
-    try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_training", "java.training", "JavaTraining1")) {
-      // Write BLOB to database
-      String sql = "update employees set resume_blob=? where email='john.doe@foo.com'";
-      PreparedStatement updateStatement = connection.prepareStatement(sql);
+    public static void main(String args[]) {
+        String dbUrl = "jdbc:mysql://localhost:3306/java_training?autoReconnect=true&useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String user = "root", pass = "root";
+        try (Connection connection = DriverManager.getConnection(dbUrl, user, pass)) {
+            // Write BLOB to database
+            String sql = "update employees set resume_blob=? where email='john.doe@foo.com'";
+            PreparedStatement updateStatement = connection.prepareStatement(sql);
 
-      File theFile = new File("./JavaSE/src/resources/sample_resume.pdf");
-      FileInputStream updateInput = new FileInputStream(theFile);
-      updateStatement.setBinaryStream(1, updateInput);
+            File theFile = new File(".\\src\\main\\resources\\sample_resume.pdf");
+            FileInputStream updateInput = new FileInputStream(theFile);
+            updateStatement.setBinaryStream(1, updateInput);
 
-      updateStatement.executeUpdate();
+            updateStatement.executeUpdate();
 
-      // Read BLOB from database
-      Statement selectStatement = connection.createStatement();
-      sql = "select resume_blob from employees where email='john.doe@foo.com'";
-      ResultSet result = selectStatement.executeQuery(sql);
+            // Read BLOB from database
+            Statement selectStatement = connection.createStatement();
+            sql = "select resume_blob from employees where email='john.doe@foo.com'";
+            ResultSet result = selectStatement.executeQuery(sql);
 
-      if (result.next()) {
-        theFile = new File("./JavaSE/src/resources/resume_from_db.pdf");
-        FileOutputStream output = new FileOutputStream(theFile);
+            if (result.next()) {
+                theFile = new File(".\\src\\main\\resources\\sample_resume.pdf");
+                FileOutputStream output = new FileOutputStream(theFile);
 
-        InputStream selectInput = result.getBinaryStream("resume_blob");
-        byte[] buffer = new byte[1024];
-        while (selectInput.read(buffer) > 0) {
-          output.write(buffer);
+                InputStream selectInput = result.getBinaryStream("resume_blob");
+                byte[] buffer = new byte[1024];
+                while (selectInput.read(buffer) > 0) {
+                    output.write(buffer);
+                }
+
+                System.out.println("\nSaved to file: " + theFile.getAbsolutePath());
+            }
+        } catch (SQLException | IOException exc) {
+            exc.printStackTrace();
         }
-
-        System.out.println("\nSaved to file: " + theFile.getAbsolutePath());
-      }
-    } catch (SQLException | IOException exc) {
-      exc.printStackTrace();
     }
-  }
 }
